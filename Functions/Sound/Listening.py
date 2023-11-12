0bc99540f6247ba250866b3a7f271a00059fe711
mmrozinski <mmrozinski@student.agh.edu.pl>
2023-11-12 18:40:32 +0100

    Audio recording update

--- Functions/Sound/Listening.py
+++ Functions/Sound/Listening.py
@@ -9,9 +9,11 @@ Functions:
 """
 import discord
 from discord.ext import commands
+import speech_recognition as sr
+from os import path
 
 
-async def once_done(sink: discord.sinks, channel: discord.TextChannel,
+async def once_done(sink: discord.sinks.Sink, channel: discord.TextChannel,
                     *args):  # Our voice client already passes these in.
     recorded_users = [  # A list of recorded users
         f"<@{user_id}>"
@@ -22,6 +24,35 @@ async def once_done(sink: discord.sinks, channel: discord.TextChannel,
              sink.audio_data.items()]  # List down the files.
     await channel.send(f"finished recording audio for: {', '.join(recorded_users)}.",
                        files=files)  # Send a message with the accumulated files.
+
+
+async def once_done_with_stt(sink: discord.sinks.Sink, channel: discord.TextChannel,
+                             *args):
+    recorded_users = [
+        f"<@{user_id}>"
+        for user_id, audio in sink.audio_data.items()
+    ]
+    await sink.vc.disconnect()
+    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in
+             sink.audio_data.items()]
+    await channel.send(f"finished recording audio for: {', '.join(recorded_users)}.")
+
+    r = sr.Recognizer()
+    file: discord.File
+    for file in files:
+        with open("tmp_audio_file.wav", "wb") as tmp_file:
+            tmp_file.write(file.fp.read())
+
+        AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "test.wav")
+        with sr.AudioFile(AUDIO_FILE) as source:
+            audio = r.record(source)
+            try:
+                recognized_text = r.recognize_google(audio, show_all=True)
+                await channel.send(f"Audio transcription: \"{recognized_text}\"")
+            except sr.UnknownValueError:
+                await channel.send(f"*Unintelligible*")
+            except sr.RequestError as e:
+                print("Transcription error; {0}".format(e))
 
 
 class Listening(commands.Cog):
@@ -47,6 +78,23 @@ class Listening(commands.Cog):
         vc.start_recording(
             discord.sinks.WaveSink(),  # The sink type to use.
             once_done,  # What to do once done.
+            ctx.channel  # The channel to disconnect from.
+        )
+        await ctx.message.reply("Started recording!")
+
+    @commands.command()
+    async def transcribe(self, ctx: commands.Context):  # If you're using commands.Bot, this will also work.
+        voice = ctx.author.voice
+
+        if not voice:
+            await ctx.message.reply("You aren't in a voice channel!")
+
+        vc: discord.VoiceClient = await voice.channel.connect()  # Connect to the voice channel the author is in.
+        self.connections.update({ctx.guild.id: vc})  # Updating the cache with the guild and channel.
+
+        vc.start_recording(
+            discord.sinks.WaveSink(),  # The sink type to use.
+            once_done_with_stt,  # What to do once done.
             ctx.channel  # The channel to disconnect from.
         )
         await ctx.message.reply("Started recording!")
