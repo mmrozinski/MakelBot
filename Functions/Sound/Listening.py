55480c11c3171c931f866259d435830f40edc39a
mmrozinski <mmrozinski@student.agh.edu.pl>
2023-11-12 23:22:21 +0100

    Add a very proof-of-concept audio conversation functionality

--- Functions/Sound/Listening.py
+++ Functions/Sound/Listening.py
@@ -7,10 +7,39 @@ Classes:
 Functions:
 
 """
+import random
+from io import BytesIO
+
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import speech_recognition as sr
 import os
+from gtts import gTTS
+
+from discord.sinks import RawData
+
+
+# This most likely is a crime against clean code, but it works
+# Anyway, later on I might want to consider changing another method further down to check which users were active last frame
+# Also, consider using a timestamp to check the duration of silence
+def my_unpack_audio(self, data):
+    if 200 <= data[1] <= 204:
+        return
+    if self.paused:
+        return
+
+    data = RawData(data, self)
+
+    if data.decrypted_data == b"\xf8\xff\xfe":
+        self.wasLastFrameSilence = True
+        return
+
+    self.wasLastFrameSilence = False
+    self.decoder.decode(data)
+
+
+discord.VoiceClient.wasLastFrameSilence = True
+discord.VoiceClient.unpack_audio = my_unpack_audio
 
 
 async def once_done(sink: discord.sinks.Sink, channel: discord.TextChannel,
@@ -47,7 +76,7 @@ async def once_done_with_stt(sink: discord.sinks.Sink, channel: discord.TextChan
         os.remove("tmp_audio_file2.wav")
         os.system(".\\ffmpeg\\ffmpeg.exe -i .\\tmp_audio_file.wav .\\tmp_audio_file_fixed.wav")
 
-        with sr.AudioFile("tmp_audio_file2.wav") as source:
+        with sr.AudioFile("tmp_audio_file_fixed.wav") as source:
             audio = r.record(source)
             try:
                 recognized_text = r.recognize_google(audio, language="en-IT")
@@ -57,15 +86,72 @@ async def once_done_with_stt(sink: discord.sinks.Sink, channel: discord.TextChan
             except sr.RequestError as e:
                 print("Transcription error; {0}".format(e))
 
+
+async def stop_listen_recording(sink: discord.sinks.Sink, ctx: commands.Context, bot: discord.Bot,
+                                vc: discord.VoiceClient,
+                                *args):
+    recorded_users = [
+        f"<@{user_id}>"
+        for user_id, audio in sink.audio_data.items()
+    ]
+    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in
+             sink.audio_data.items()]
+
+    r = sr.Recognizer()
+
+    transcriptions = []
+
+    file: discord.File
+
+    for file in files:
+        with open("tmp_audio_file.wav", "wb") as tmp_file:
+            tmp_file.write(file.fp.read())
+
+        os.system(".\\ffmpeg\\ffmpeg.exe -y -i .\\tmp_audio_file.wav .\\tmp_audio_file_fixed.wav")
+
+        with sr.AudioFile("tmp_audio_file_fixed.wav") as source:
+            audio = r.record(source)
+            try:
+                recognized_text = r.recognize_google(audio, language="en-IT")
+                transcriptions.append(recognized_text)
+                print(recognized_text)
+            except sr.UnknownValueError:
+                pass
+
+    if len(transcriptions) > 0:
+        await ctx.invoke(bot.get_command("talk"), random.choice(transcriptions))
+
+        to_say = (await ctx.channel.history(limit=1).flatten())[0].content
+        voice = gTTS(text=to_say, lang="en", slow=False)
+
+        voice.save("tmp_speech_file.mp3")
+
+        dc_audio = discord.FFmpegPCMAudio(source="tmp_speech_file.mp3",
+                                          executable="C:\\Users\\Makel\\PycharmProjects\\MakelBot\\ffmpeg\\ffmpeg.exe")
+
+        vc.play(dc_audio)
+
+    vc.start_recording(
+        discord.sinks.WaveSink(),
+        stop_listen_recording,
+        ctx,
+        bot,
+        vc
+    )
+
+
 class Listening(commands.Cog):
     bot: commands.Bot = None
     connections = {}
+    listen_connections_silence = {}
 
     def __init__(self, bot):
         self.system = None
         self.context = None
         self.bot = bot
         self._last_member = None
+
+        self.listen.start()
 
     @commands.command()
     async def record(self, ctx: commands.Context):  # If you're using commands.Bot, this will also work.
@@ -110,6 +196,37 @@ class Listening(commands.Cog):
             await ctx.message.delete()  # And delete.
         else:
             await ctx.message.reply("I am currently not recording here.")  # Respond with this if we aren't recording.
+
+    @commands.command()
+    async def start_listening(self, ctx: commands.Context):
+        voice = ctx.author.voice
+
+        if not voice:
+            await ctx.message.reply("You aren't in a voice channel!")
+
+        vc: discord.VoiceClient = await voice.channel.connect(timeout=float("inf"))
+        self.connections.update({ctx.guild.id: vc})
+        self.listen_connections_silence.update({ctx.guild.id: [vc, False]})
+
+        vc.start_recording(
+            discord.sinks.WaveSink(),
+            stop_listen_recording,
+            ctx,
+            self.bot,
+            vc
+        )
+        await ctx.message.reply("Started listening!")
+
+    @tasks.loop(seconds=1)
+    async def listen(self):
+        for guild in self.listen_connections_silence.keys():
+            vc = self.listen_connections_silence[guild][0]
+            if self.listen_connections_silence[guild][1]:
+                self.listen_connections_silence[guild][1] = vc.wasLastFrameSilence
+                if self.listen_connections_silence[guild][1] and vc.recording:
+                    vc.stop_recording()
+            else:
+                self.listen_connections_silence[guild][1] = vc.wasLastFrameSilence
 
 
 def setup(bot):
