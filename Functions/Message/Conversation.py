fb57ab2f26cae561f8d9e979faf35385e74d85af
mmrozinski <mmrozinski@student.agh.edu.pl>
2024-01-17 22:27:37 +0100

    More deranged voice conversation development

--- Functions/Message/Conversation.py
+++ Functions/Message/Conversation.py
@@ -10,7 +10,193 @@ Functions:
 import discord
 from discord.ext import commands
 import requests
-from Local.Configs.Servers import OLLAMA_SERVER
+from Local.Configs.Servers import OLLAMA_SERVER, KOBOLD_SERVER, FASTAPI_SERVER
+from transformers import PreTrainedTokenizerFast, LlamaTokenizerFast
+import os
+
+
+class FastAPIConversationLllama2:  # TODO: this is a duplicate from Conversation - solve this sometime
+    T_START = "[INST]"
+    T_SYSTEM = "<<SYS>>"
+    T_SYSTEM_END = "<</SYS>>"
+    T_END = "[/INST]"
+
+    def __init__(self):
+        self.api_url = FASTAPI_SERVER
+        self.session = requests.Session()
+        self.tokenizer = LlamaTokenizerFast.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=True)
+        prompt_file = open(os.path.join(os.path.dirname(__file__), "prompt_fastapi.txt"), "r")
+        self.system = prompt_file.read()
+        self.system_tokens = len(self.tokenizer(self.system).input_ids)
+        prompt_file.close()
+        self.history = []
+        self.max_context_len = 3900
+
+        self.is_first = True
+
+        self.reload()
+
+    def generate(self, prompt: str) -> requests.Response:
+        self.history.append((prompt, "user", len(self.tokenizer(prompt).input_ids)))
+
+        context_len = self.system_tokens + sum(list(zip(*self.history))[2])
+
+        while context_len > self.max_context_len:
+            self.history.pop(0)
+            self.history.pop(0)
+            context_len = self.system_tokens + sum(list(zip(*self.history))[2])
+
+        prompt_to_send = self.prompt_from_history()
+
+        response = self.session.get(self.api_url + "/?prompt=" + prompt_to_send, stream=True)
+
+        if response.status_code != 200:
+            raise requests.HTTPError(response)
+
+        return response
+
+    def add_to_history(self, text: str, sender: str):
+        self.history.append((text, sender, len(self.tokenizer(text).input_ids)))
+
+    def prompt_from_history(self):
+        prompt = ""
+
+        if self.system is not None:
+            prompt += self.system
+
+        for msg in self.history:
+            if msg[1] == "system":
+                prompt += (FastAPIConversationLllama2.T_START + " " + FastAPIConversationLllama2.T_SYSTEM + "\n" +
+                           msg[0] + "\n" + FastAPIConversationLllama2.T_SYSTEM_END + "\n" +
+                           FastAPIConversationLllama2.T_END + "\n")
+            elif msg[1] == "user":
+                prompt += FastAPIConversationLllama2.T_START + msg[0] + FastAPIConversationLllama2.T_END + "\n"
+            elif msg[1] == "model":
+                prompt += msg[0] + "\n"
+
+        return prompt
+
+    def reload(self):
+        response = requests.get(self.api_url + "/reset")
+
+        if response.status_code != 200:
+            raise requests.HTTPError(response)
+
+        self.is_first = True
+
+class KoboldConversationPygmalion2:
+    T_SYSTEM = "<|system|>"
+    T_USER = "<|user|>"
+    T_MODEL = "<|model|>"
+
+    def __init__(self):
+        self.api_url = KOBOLD_SERVER + "/api/v1"
+        self.options = {
+            "use_userscripts": True,
+            "stop_sequence": ["###", "\n", KoboldConversationPygmalion2.T_USER, "<|"]
+        }
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained("PygmalionAI/pygmalion-2-7b")
+        prompt_file = open(os.path.join(os.path.dirname(__file__), "prompt_kobold.txt"), "r")
+        self.system = prompt_file.read()
+        self.history = []
+        self.max_context_len = 2048
+
+        self.reload()
+
+    def generate_from_history(self) -> str:
+        data = dict(self.options)
+
+        context_len = sum(list(zip(*self.history))[2])
+
+        if context_len > self.max_context_len:
+            self.history.pop(0)
+            self.history.pop(0)
+
+        data["prompt"] = self.prompt_from_history()
+
+        response = requests.post(self.api_url + "/generate", json=data)
+
+        if response.status_code != 200:
+            self.history.pop()
+            raise requests.HTTPError(response)
+
+        result = response.json()["results"][0]["text"].removesuffix("<|")
+
+        self.history.append((result, "model", len(self.tokenizer(result).input_ids)))
+
+        return result
+
+    def generate(self, prompt: str) -> str:
+        data = dict(self.options)
+
+        self.history.append((prompt, "user", len(self.tokenizer(prompt).input_ids)))
+
+        context_len = sum(list(zip(*self.history))[2])
+
+        while context_len > self.max_context_len:
+            self.history.pop(0)
+            self.history.pop(0)
+            context_len = sum(list(zip(*self.history))[2])
+
+        data["prompt"] = self.prompt_from_history()
+
+        response = requests.post(self.api_url + "/generate", json=data)
+
+        if response.status_code != 200:
+            self.history.pop()
+            raise requests.HTTPError(response)
+
+        result = response.json()["results"][0]["text"].removesuffix("<|")
+
+        self.history.append((result, "model", len(self.tokenizer(result).input_ids)))
+
+        return result
+
+    def prompt_from_history(self):
+        prompt = ""
+
+        if self.system is not None:
+            prompt += KoboldConversationPygmalion2.T_SYSTEM + self.system
+
+        for msg in self.history:
+            if msg[1] == "system":
+                prompt += KoboldConversationPygmalion2.T_SYSTEM + msg[0]
+            elif msg[1] == "user":
+                prompt += KoboldConversationPygmalion2.T_USER + msg[0]
+            elif msg[1] == "model":
+                prompt += KoboldConversationPygmalion2.T_MODEL + msg[0]
+
+        prompt += KoboldConversationPygmalion2.T_MODEL
+
+        return prompt
+
+    def reload(self):
+        data = {"name": "Robo default"}
+        response = requests.put(self.api_url + "/story/load", json=data)
+
+        if response.status_code != 200:
+            raise requests.HTTPError(response)
+
+        # data = {"model": "PygmalionAI/pygmalion-2-7b"}
+        #
+        # response = requests.get(self.api_url + "/model")
+        #
+        # if response.status_code != 200:
+        #     raise requests.HTTPError(response)
+        #
+        # current_model = response.json()["result"]
+        #
+        # if current_model != data["model"]:
+        #     response = requests.put(self.api_url + "/model", json=data)
+        #
+        #     if response.status_code != 200:
+        #         raise requests.HTTPError(response)
+
+
+class GenerationServers:
+    Kobold = "kobold"
+    Ollama = "ollama"
+    FastAPI = "fastapi"
 
 
 class Conversation(commands.Cog):
@@ -31,6 +217,12 @@ class Conversation(commands.Cog):
         self._last_member = None
         self.conv_channels = set()
         self.options = {}
+        self.model = "test1"
+        self.server = GenerationServers.FastAPI
+        if self.server == GenerationServers.Kobold:
+            self.kobold = KoboldConversationPygmalion2()
+        elif self.server == GenerationServers.FastAPI:
+            self.fastAPI = FastAPIConversationLllama2()
 
     conv_context_buffer = ""
 
@@ -44,28 +236,68 @@ class Conversation(commands.Cog):
 
             prompt = prompt[1:]
 
-            data = {
-                "model": "llama2:13b-chat",
-                "prompt": prompt,
-                "context": self.context,
-                "stream": False,
-                "options": self.options
-            }
+            if self.server == GenerationServers.Ollama:
 
-            if self.system is not None:
-                data["system"] = self.system
+                data = {
+                    "model": self.model,
+                    "prompt": prompt,
+                    "context": self.context,
+                    "stream": False,
+                    "options": self.options
+                }
 
-            response = requests.post("http://" + OLLAMA_SERVER + "/api/generate", json=data)
+                if self.system is not None and self.context is None:
+                    data["system"] = self.system
 
-            response_json = response.json()
-            self.context = response_json["context"]
+                response = requests.post(OLLAMA_SERVER + "/api/generate", json=data)
 
-        await ctx.channel.send(response_json["response"])
+                response_json = response.json()
+                self.context = response_json["context"]
+
+                await ctx.channel.send(response_json["response"])
+
+            elif self.server == GenerationServers.Kobold:
+                result = self.kobold.generate(prompt)
+                await ctx.channel.send(result)
+
+            elif self.server == GenerationServers.FastAPI:
+                response = self.fastAPI.generate(prompt)
+                text = ""
+                pause_marks = (".", ",", "!", "?")
+                for token in response.iter_lines():
+                    token_text = token.decode("utf-8")
+                    text += token_text
+                    for mark in pause_marks:
+                        if mark in token_text:
+                            await ctx.channel.send(text)
+                            text = ""
+
+    @commands.command()
+    async def talk_without_generation(self, ctx: commands.Context, *args):
+        prompt = ""
+        for arg in args:
+            prompt += " "
+            prompt += arg
+
+        prompt = prompt[1:]
+
+        if self.server == GenerationServers.Kobold:
+            self.kobold.history.append((prompt, "user", len(self.kobold.tokenizer(prompt).input_ids)))
+
+    @commands.command()
+    async def talk_generate(self, ctx: commands.Context):
+        async with ctx.channel.typing():
+            if self.server == GenerationServers.Kobold:
+                result = self.kobold.generate_from_history()
+                await ctx.channel.send(result)
 
     @commands.command()
     async def talk_reset(self, ctx):
-        self.context = None
-        self.system = None
+        if self.server == GenerationServers.Ollama:
+            self.context = None
+            self.system = None
+        elif self.server == GenerationServers.Kobold:
+            self.kobold.reload()
         await ctx.channel.send("Successfully reset the conversation context and system prompt!")
 
     @commands.command()
--- Functions/Sound/Listening.py
+++ Functions/Sound/Listening.py
@@ -10,17 +10,113 @@ Functions:
 import random
 
 import discord
+import pyttsx3
 from discord.ext import commands, tasks
 import speech_recognition as sr
 import os
-from gtts import gTTS
 from pydub import AudioSegment
 from discord.sinks import RawData
+from faster_whisper import WhisperModel
+import time
+import requests
+from Local.Configs.Servers import FASTAPI_SERVER
+from transformers import LlamaTokenizerFast
+import asyncio
 
+import socket
+import requests.packages.urllib3.util.connection as urllib3_cn
+
+
+def allowed_gai_family():
+    """
+     https://github.com/shazow/urllib3/blob/master/urllib3/util/connection.py
+    """
+    family = socket.AF_INET
+    if urllib3_cn.HAS_IPV6:
+        family = socket.AF_INET  # force ipv4 only if it is available
+    return family
+
+
+urllib3_cn.allowed_gai_family = allowed_gai_family
 
 if os.name == "nt":
     AudioSegment.converter = os.path.dirname(__file__) + "\\..\\..\\ffmpeg\\ffmpeg.exe"
 
+tts_engine = pyttsx3.init()
+rate = tts_engine.getProperty('rate')
+tts_engine.setProperty("rate", rate + 50)
+
+
+class FastAPIConversationNoxus:
+    T_START = "<|im_start|>"
+    T_END = "<|im_end|>"
+
+    def __init__(self):
+        self.api_url = FASTAPI_SERVER
+        self.session = requests.Session()
+        self.tokenizer = LlamaTokenizerFast.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=True)
+        prompt_file = open(os.path.join(os.path.dirname(__file__), "prompt_fastapi_guanaco.txt"), "r")
+        self.system = prompt_file.read()
+        self.system_tokens = len(self.tokenizer(self.system).input_ids)
+        prompt_file.close()
+        self.history = []
+        self.max_context_len = 3900
+
+        self.name = "Robo"
+
+        self.is_first = True
+
+        self.reload()
+
+    def generate(self, prompt: str) -> requests.Response:
+        self.history.append((prompt, "user", len(self.tokenizer(prompt).input_ids)))
+
+        context_len = self.system_tokens + sum(list(zip(*self.history))[2])
+
+        while context_len > self.max_context_len:
+            self.history.pop(0)
+            self.history.pop(0)
+            context_len = self.system_tokens + sum(list(zip(*self.history))[2])
+
+        prompt_to_send = self.prompt_from_history()
+
+        response = self.session.get(self.api_url + "/?prompt=" + prompt_to_send, stream=True)
+
+        if response.status_code != 200:
+            raise requests.HTTPError(response)
+
+        return response
+
+    def add_to_history(self, text: str, sender: str):
+        self.history.append((text, sender, len(self.tokenizer(text).input_ids)))
+
+    def prompt_from_history(self):
+        prompt = ""
+
+        if self.system is not None:
+            prompt += FastAPIConversationNoxus.T_START + "system\n" + self.system + FastAPIConversationNoxus.T_END + "\n"
+
+        for msg in self.history[:]:
+            if msg[1] == "system":
+                prompt += FastAPIConversationNoxus.T_START + "system\n" + msg[0] + FastAPIConversationNoxus.T_END + "\n"
+            elif msg[1] == "user":
+                prompt += FastAPIConversationNoxus.T_START + "user\n" + msg[0] + FastAPIConversationNoxus.T_END + "\n"
+            elif msg[1] == "model":
+                prompt += FastAPIConversationNoxus.T_START + "assistant\n" + msg[0] + FastAPIConversationNoxus.T_END + "\n"
+
+        prompt += FastAPIConversationNoxus.T_START + "assistant\n"
+
+        print(prompt)
+
+        return prompt
+
+    def reload(self):
+        response = requests.get(self.api_url + "/reset")
+
+        if response.status_code != 200:
+            raise requests.HTTPError(response)
+
+        self.is_first = True
 
 # This most likely is a crime against clean code, but it works
 # Anyway, later on I might want to consider changing another method further down to check which users were active last frame
@@ -44,6 +140,9 @@ def my_unpack_audio(self, data):
 discord.VoiceClient.wasLastFrameSilence = True
 discord.VoiceClient.unpack_audio = my_unpack_audio
 
+whisper_model = WhisperModel("medium", device="cuda")
+
+llm_api = FastAPIConversationNoxus()
 
 async def once_done(sink: discord.sinks.Sink, channel: discord.TextChannel,
                     *args):  # Our voice client already passes these in.
@@ -96,10 +195,6 @@ async def once_done_with_stt(sink: discord.sinks.Sink, channel: discord.TextChan
 async def stop_listen_recording(sink: discord.sinks.Sink, ctx: commands.Context, bot: discord.Bot,
                                 vc: discord.VoiceClient,
                                 *args):
-    recorded_users = [
-        f"<@{user_id}>"
-        for user_id, audio in sink.audio_data.items()
-    ]
     files_users = [(discord.File(audio.file, f"{user_id}.{sink.encoding}"), user_id) for user_id, audio in
                    sink.audio_data.items()]
 
@@ -119,46 +214,104 @@ async def stop_listen_recording(sink: discord.sinks.Sink, ctx: commands.Context,
                 continue
 
             username = bot.get_user(user_id).display_name
-            with open("tmp_audio_file.wav", "wb") as tmp_file:
+            with open(os.path.join(os.path.dirname(__file__), "tmp_audio_file.wav"), "wb") as tmp_file:
                 tmp_file.write(file.fp.read())
 
-            if os.name == "nt":
-                os.system(".\\ffmpeg\\ffmpeg.exe -y -i .\\tmp_audio_file.wav .\\tmp_audio_file_fixed.wav")
-            elif os.name == "posix":
-                os.system("ffmpeg -y -i ./tmp_audio_file.wav ./tmp_audio_file_fixed.wav")
+            start_t = time.time()
 
-            with sr.AudioFile("tmp_audio_file_fixed.wav") as source:
-                audio = r.record(source)
-                try:
-                    recognized_text = r.recognize_google(audio, language="pl-PL")
-                    transcriptions.append((recognized_text, username))
-                    print(recognized_text)
-                except sr.UnknownValueError:
-                    pass
+            segments, _ = whisper_model.transcribe(
+                audio=os.path.join(os.path.dirname(__file__), "tmp_audio_file.wav"), vad_filter=True)
+
+            end_t = time.time()
+
+            print("transcription time: ", (end_t - start_t) * 10**3)
+
+            recognized_text = ""
+
+            for segment in segments:
+                if segment.text is not None and segment.text != "":
+                    recognized_text += segment.text
+
+            if recognized_text != "":
+                transcriptions.append((recognized_text, username))
+
+            # with sr.AudioFile("tmp_audio_file_fixed.wav") as source:
+            #     audio = r.record(source)
+            #     try:
+            #         recognized_text = r.recognize_google(audio, language="pl-PL")
+            #         transcriptions.append((recognized_text, username))
+            #         print(recognized_text)
+            #     except sr.UnknownValueError:
+            #         pass
 
         if len(transcriptions) > 0:
-            pick = random.choice(transcriptions)
-            speaker = pick[1]
-            prompt = speaker + ": " + pick[0]
-            await ctx.channel.send(prompt)
-            await ctx.invoke(bot.get_command("talk"), prompt)
+            # pick = random.choice(transcriptions)
+            prompt = ""
+            for pick in transcriptions:
+                speaker = pick[1]
+                prompt += speaker + ": " + pick[0] + "\n"
+            # await ctx.channel.send(prompt)
 
-            to_say = (await ctx.channel.history(limit=1).flatten())[0].content
-            voice = gTTS(text=to_say, lang="cs", slow=False)
+            start_t = time.time()
+            response = llm_api.generate(prompt)
 
-            voice.save("tmp_speech_file.mp3")
+            model_response = ""
 
-            # audio = AudioSegment.from_mp3("tmp_speech_file.mp3")
-            # audio.speedup(playback_speed=1.5)
-            # audio.export("tmp_speech_file.mp3", format="mp3")
+            text = ""
+            last_play = None
+            pause_marks = (".", "!", "?")
+            stop_marks = ("[INST]",)
 
-            if os.name == "nt":
-                dc_audio = discord.FFmpegPCMAudio(source="tmp_speech_file.mp3",
-                                                  executable=os.path.dirname(__file__) + "\\..\\..\\ffmpeg\\ffmpeg.exe")
-            else:
-                dc_audio = discord.FFmpegPCMAudio(source="tmp_speech_file.mp3")
+            stop = False
+            for token in response.iter_lines():
+                token_text = token.decode("utf-8")
+                text += token_text
+                for mark in pause_marks:
+                    if mark in token_text:
+                        if last_play is not None:
+                            await last_play
+                        else:
+                            end_t = time.time()
+                            print("generation time: ", (end_t - start_t) * 10 ** 3)
 
-            vc.play(dc_audio)
+                        last_play = asyncio.create_task(do_tts(vc, text))
+
+                        #await ctx.channel.send(text)
+                        model_response += text
+
+                        text = ""
+                        break
+
+            llm_api.add_to_history(model_response, "model")
+
+            # for line in transcriptions:
+            #     speaker = line[1]
+            #     prompt = speaker + ": " + line[0]
+            #     await ctx.channel.send(prompt)
+            #
+            #     start_t = time.time()
+            #
+            #     await ctx.invoke(bot.get_command("talk_without_generation"), prompt)
+            #     await ctx.invoke(bot.get_command("talk_generate"))
+            #
+            #     end_t = time.time()
+            #
+            #     print("response gen time: ", (end_t - start_t) * 10 ** 3)
+            #
+            # to_say = (await ctx.channel.history(limit=1).flatten())[0].content
+            #
+            # start_t = time.time()
+            #
+            # tts_engine.save_to_file(text=to_say, filename="tmp_speech_file.mp3")
+            # tts_engine.runAndWait()
+            #
+            # end_t = time.time()
+            #
+            # print("speech gen time: ", (end_t - start_t) * 10 ** 3)
+            #
+            # dc_audio = discord.FFmpegPCMAudio(source="tmp_speech_file.mp3")
+            #
+            # vc.play(dc_audio)
 
     vc.start_recording(
         discord.sinks.WaveSink(),
@@ -167,6 +320,27 @@ async def stop_listen_recording(sink: discord.sinks.Sink, ctx: commands.Context,
         bot,
         vc
     )
+
+voice_queue = []
+
+async def play_tts(vc: discord.VoiceClient):
+    while vc.is_playing():
+        await asyncio.sleep(0.1)
+
+    with open("tmp_playing_speech_file.mp3", "wb") as copy_file:
+        copy_file.write(voice_queue.pop(0))
+
+    dc_audio = discord.FFmpegOpusAudio(source="tmp_playing_speech_file.mp3")
+
+    vc.play(dc_audio)
+
+
+async def do_tts(vc: discord.VoiceClient, text: str):
+    tts_engine.save_to_file(text=text, filename=f"tmp_speech_file.mp3")
+    tts_engine.runAndWait()
+    with open("tmp_speech_file.mp3", "rb") as file:
+        voice_queue.append(file.read())
+    asyncio.create_task(play_tts(vc))
 
 
 class Listening(commands.Cog):
@@ -264,7 +438,7 @@ class Listening(commands.Cog):
         else:
             await ctx.message.reply("I am currently not listening here.")
 
-    @tasks.loop(seconds=0.5)
+    @tasks.loop(seconds=0.25)
     async def listen(self):
         for guild in self.listen_connections_silence.keys():
             vc = self.listen_connections_silence[guild][0]
